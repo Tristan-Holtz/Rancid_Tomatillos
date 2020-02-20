296af0110eeb3b78508ffd749b96c152075055e4
Tristan Holtz <TristanHoltzConsulting@gmail.com>
2020-02-19 21:45:23 -0700

    Create Login component with basic styling

---
+++ src/components/Login/Login.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import './Login.css';
+
+export default class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      email: '',
+      password: ''
+    };
+  }
+
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  render() {
+    return (
+      <form className="login-form">
+        <label htmlFor="login-name">Name</label>
+        <input
+          onChange={this.handleChange}
+          name="name"
+          id="login-name"
+          placeholder="Name"
+        ></input>
+        <label htmlFor="login-email">Email</label>
+        <input
+          onChange={this.handleChange}
+          name="email"
+          id="login-email"
+          placeholder="Email"
+        ></input>
+        <label htmlFor="login-password">Password</label>
+        <input
+          onChange={this.handleChange}
+          name="password"
+          id="login-password"
+          placeholder="Password"
+        ></input>
+        <button>Login</button>
+      </form>
+    );
+  }
+}
