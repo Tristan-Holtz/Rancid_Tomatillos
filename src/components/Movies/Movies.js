20fed5648a23310d057332ec6cdc9b57926f53d3
Ryan Novak <ryan.novak95@yahoo.com>
2020-02-19 22:51:03 -0700

    Added rough outline for new components, also added new component folder and files, as well as .css files

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import './Movies.css';
+import MovieDetails from '../MovieDetails/MovieDetails'
 
 class Movies extends Component {
   constructor() {
@@ -20,17 +22,22 @@ class Movies extends Component {
   };
 
   render() {
+    // for the time being, toggle the line of code below as active/inactive
+    // to switch between different components rendering
     const { movies } = this.state;
+    return <MovieDetails movies={movies}/>
     const movieCards = movies.map(movie => {
       return (
-        <article key={movie.id}>
+        <article className='movie-card' key={movie.id}>
+          <img className='movie-card-image' src={movie.poster_path}/>
           <h1>{movie.title}</h1>
-          <p>{movie.release_date}</p>
-          <p>{movie.overview}</p>
+          <h3>Avg. rating: {movie.average_rating}</h3>
+          {/* <p>{movie.release_date}</p> */}
+          {/* <p>{movie.overview}</p> */}
         </article>
       );
     });
-    return <section>{movieCards}</section>;
+    return <section className='movie-cards-section'>{movieCards}</section>;
   }
 }
 
