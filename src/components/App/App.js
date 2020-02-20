c38ceb57c2ca33f482bf24ce1a65b0338ed0a612
Tristan Holtz <TristanHoltzConsulting@gmail.com>
2020-02-19 22:22:44 -0700

    Create Header

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Movies from '../Movies/Movies';
 import Login from '../Login/Login';
+import Header from '../Header/Header';
 
 class App extends Component {
   render() {
     return (
       <main>
+        <Header />
         <Login />
         <Movies />
       </main>
