ba0c8b073b04a0fba3679579ed760fdba80f1fe9
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2020-02-20 16:33:35 -0700

    Add routes to App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,26 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Movies from '../Movies/Movies';
 import Login from '../Login/Login';
 import Header from '../Header/Header';
+import MovieDetails from '../MovieDetails/MovieDetails';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <main>
-        <Header />
-        <Login />
-        <Movies />
-      </main>
-    );
+      <Router>
+        <main>
+          <Switch>
+            <Route exact path='/login'   component={Login} />
+            <Route path='/' component={Header} />
+          </Switch>
+          <Route exact path='/' component={Movies} />
+          <Route exact path='/movies/:id' component={MovieDetails}/>
+        </main>
+      </Router>
+    )
   }
 }
 
