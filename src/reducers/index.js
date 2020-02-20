380bc1ffe0584046de9dcea390d8b704f88e224d
Tristan Holtz <TristanHoltzConsulting@gmail.com>
2020-02-20 15:07:29 -0700

    Fix git conflict

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { moviesReducer } from '../reducers/reducers';
 import { combineReducers } from 'redux';
 
@@ -7,11 +6,3 @@ const rootReducer = combineReducers({
 });
 
 export default rootReducer;
-=======
-import { combineReducers } from 'redux';
-// // import { }
-
-const rootReducer = combineReducers({})
-
-export default rootReducer;
->>>>>>> master
