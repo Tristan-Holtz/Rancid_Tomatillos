702165612c39f0843d84aa71d3751e8a86546fcd
Tristan Holtz <TristanHoltzConsulting@gmail.com>
2020-02-20 16:36:04 -0700

    Update fetch to async

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 import Movies from '../Movies/Movies';
 import Login from '../Login/Login';
 import Header from '../Header/Header';
--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,58 +1,60 @@
 import React, { Component } from 'react';
 import './Movies.css';
 import { connect } from 'react-redux';
-// import { getData } from '../../actions/actions';
+import { setMovies } from '../../actions/actions';
 import MovieDetails from '../MovieDetails/MovieDetails';
 
 class Movies extends Component {
   constructor() {
     super();
-    this.state = {
-      movies: []
-    };
+    this.state = {};
   }
 
-  getData = () => {
-    fetch('https://rancid-tomatillos.herokuapp.com/api/v1/movies')
+  getMovies = async () => {
+    return fetch('https://rancid-tomatillos.herokuapp.com/api/v1/movies')
       .then(response => response.json())
-      .then(movies => this.setState({ movies: movies.movies }))
-      .catch(error => console.log(error));
+      .then(movies => movies.movies);
   };
 
-  componentDidMount = () => {
-    this.getData();
-  };
+  async componentDidMount() {
+    try {
+      const movies = await this.getMovies();
+      this.props.setMovies(movies);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   render() {
     // for the time being, toggle the line of code below as active/inactive
     // to switch between different components rendering
-    const { movies } = this.state;
+    const { movies } = this.props;
     return <MovieDetails movies={movies} />;
-    const movieCards = movies.map(movie => {
-      return (
-        <article className="movie-card" key={movie.id}>
-          <img className="movie-card-image" src={movie.poster_path} />
-          <h1>{movie.title}</h1>
-          <h3>Avg. rating: {movie.average_rating}</h3>
-          {/* <p>{movie.release_date}</p> */}
-          {/* <p>{movie.overview}</p> */}
-        </article>
-      );
-    });
-    return <section className="movie-cards-section">{movieCards}</section>;
+    // const movieCards = movies.map(movie => {
+    //   return (
+    //     <article className="movie-card" key={movie.id}>
+    //       <img className="movie-card-image" src={movie.poster_path} />
+    //       <h1>{movie.title}</h1>
+    //       <h3>Avg. rating: {movie.average_rating}</h3>
+    //       {/* <p>{movie.release_date}</p> */}
+    //       {/* <p>{movie.overview}</p> */}
+    //     </article>
+    //   );
+    // });
+    // return <section className="movie-cards-section">{movieCards}</section>;
   }
 }
 
-// const mapStateToProps = state => ({
-//   movies: state.movies
-// });
+const mapStateToProps = state => ({
+  movies: state.movies
+});
 
-// const mapDispatchToProps = dispatch => ({
-//   getData: () => {
-//     dispatch(getData());
-//   }
-// });
+const mapDispatchToProps = dispatch => ({
+  setMovies: movies => {
+    dispatch(setMovies(movies));
+  }
+});
 
-// export default connect(mapStateToProps, mapDispatchToProps)(Movies);
+export default connect(mapStateToProps, mapDispatchToProps)(Movies);
 
-export default Movies;
+// export default Movies;
