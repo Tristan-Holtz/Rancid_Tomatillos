96859740ecd9a84d8c3251cd3ecc10c415723989
Tristan Holtz <TristanHoltzConsulting@gmail.com>
2020-02-20 16:32:50 -0700

    Setup movies reducer

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,9 +1,4 @@
-export const getData = () => {
-  fetch('https://rancid-tomatillos.herokuapp.com/api/v1/movies')
-    .then(response => response.json())
-    .then(movies => ({
-      type: 'GET_MOVIES',
-      movies: movies.movies
-    }))
-    .catch(error => console.log(error));
-};
+export const setMovies = movies => ({
+  type: 'SET_MOVIES',
+  movies: movies
+});
