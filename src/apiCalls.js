5766dcb91bec18da58df4e9af1b2d9faf83aeac6
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2020-02-21 23:41:21 -0700

    Refactor getUser to include url and options

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,9 +1,19 @@
-export const getUser = async (url, options) => {
-  return await fetch(url, options)
-    .then(res => {
-      if (!res.ok) {
-        throw Error("Error! No 200 Status Code Found.")
-      }
-      return res.json()
-    })
+export const getUser = async (email, password) => {
+  const url = 'https://rancid-tomatillos.herokuapp.com/api/v1/login';
+  const options = {
+    method: 'POST',
+    body: JSON.stringify({
+      email,
+      password
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }
+  const res = await fetch(url, options)
+  if (!res.ok) {
+    throw new Error("Error! No 200 Status Code Found.")
+  }
+  const user = await res.json();
+  return user
 }
