ca0652af78930e31a502548bf78baf748385d350
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2020-02-21 18:03:40 -0700

    Add apiCalls file

---
+++ src/apiCalls.js
@@ -0,0 +1,11 @@
+export const getUser = async (url, options) => {
+  return await fetch(url, options)
+    .then(res => {
+      if (!res.ok) {
+        throw Error("Error! No 200 Status Code Found.")
+      }
+      return res.json()
+    })
+    .then(user => {return user})
+    .catch(error => console.log(error))
+}
