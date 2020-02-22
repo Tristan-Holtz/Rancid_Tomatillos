07160b532ee600b45ff29eb2a0d3d63eebc320dd
Tristan Holtz <TristanHoltzConsulting@gmail.com>
2020-02-22 13:54:14 -0700

    Setup for movie details page

--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './MovieDetails.css';
 
-const MovieDetails = ({ movies }) => {
+const MovieDetails = ({ location }) => {
+  const movie = location.state;
   return (
     <section className="card-detail-section">
-      <h1>MOVIE TITLE</h1>
-      <h3>MOVIE AVERAGE RATING</h3>
-      <p>MOVIE OVERVIEW</p>
+      <img src={movie.backdrop_path} alt={movie.title + ' backdrop'} />
+      <img src={movie.poster_path} alt={movie.title + ' poster'} />
+      <h1>{movie.title}</h1>
+      <p>{movie.release_date}</p>
+      <h3>{movie.average_rating}</h3>
+      <p>{movie.overview}</p>
     </section>
   );
 };
