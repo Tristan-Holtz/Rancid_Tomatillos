6ea36eca96654cec6296a5f16e025ad4c3fbfb88
Tristan Holtz <TristanHoltzConsulting@gmail.com>
2020-02-22 15:15:20 -0700

    Style movie details page

--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -1,16 +1,35 @@
 import React from 'react';
-import './MovieDetails.css';
+import './MovieDetails.scss';
 
 const MovieDetails = ({ location }) => {
   const movie = location.state;
   return (
     <section className="card-detail-section">
-      <img src={movie.backdrop_path} alt={movie.title + ' backdrop'} />
-      <img src={movie.poster_path} alt={movie.title + ' poster'} />
-      <h1>{movie.title}</h1>
-      <p>{movie.release_date}</p>
-      <h3>{movie.average_rating}</h3>
-      <p>{movie.overview}</p>
+      <div className="movie-main">
+        <img
+          className="movie-backdrop"
+          src={movie.backdrop_path}
+          alt={movie.title + ' backdrop'}
+        />
+        <div className="overview-container">
+          <label className="movie-overview">Overview</label>
+          <p className="movie-description">{movie.overview}</p>
+        </div>
+      </div>
+      <article className="movie-stats-container">
+        <img
+          className="movie-poster"
+          src={movie.poster_path}
+          alt={movie.title + ' poster'}
+        />
+        <div className="movie-stats">
+          <h1 className="movie-title">{movie.title}</h1>
+          <p className="movie-date">Release Date {movie.release_date}</p>
+          <h3 className="movie-rating">
+            Average Rating {movie.average_rating}
+          </h3>
+        </div>
+      </article>
     </section>
   );
 };
