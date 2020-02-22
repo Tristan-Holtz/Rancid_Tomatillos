9f58259a9e0632f36536fb5330c93786988f9638
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2020-02-21 18:06:17 -0700

    Remove connect import

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { connect } from 'react-redux';
 import Movies from '../Movies/Movies';
 import Login from '../Login/Login';
 import Header from '../Header/Header';
