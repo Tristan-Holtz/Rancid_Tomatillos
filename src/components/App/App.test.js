601d08e1367ee36047f2682fd30d44c933624c1f
Tristan Holtz <TristanHoltzConsulting@gmail.com>
2020-02-23 12:59:54 -0700

    Write snapshot test for App

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,9 +1,11 @@
-// import React from 'react';
-// import { render } from '@testing-library/react';
-// import App from './App';
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './App';
 
-// test('renders learn react link', () => {
-//   const { getByText } = render(<App />);
-//   const linkElement = getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+describe('App', () => {
+  it('should render the component', () => {
+    const wrapper = shallow(<App />);
+
+    expect(wrapper).toMatchSnapshot();
+  });
+});
