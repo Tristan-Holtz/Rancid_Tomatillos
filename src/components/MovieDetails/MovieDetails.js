4fa8a6f426f50db200536d8a60533841acb7c145
Ryan Novak <ryan.novak95@yahoo.com>
2020-02-24 08:15:32 -0700

    added go back button on movie details page

--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './MovieDetails.scss';
 
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { setMovies, setRatings } from '../../actions/actions';
 import { addUserRating, getRatings, deleteRating } from '../../apiCalls';
 
@@ -60,6 +61,7 @@ export const MovieDetails = ({ location, user, ratings, setRatings }) => {
   const movie = location.state;
   return (
     <section className="card-detail-section">
+      <Link to= '/'>Go back</Link>
       <div className="movie-main">
         <img
           className="movie-backdrop"
@@ -107,45 +109,3 @@ const mapDispatchToProps = dispatch => ({
 
 
 export default connect(mapStateToProps, mapDispatchToProps)(MovieDetails);
-
-
-
-// an original copy of this component is below incase switching to a class based component messes things up.
-
-// import React from 'react';
-// import './MovieDetails.scss';
-
-// const MovieDetails = ({ location }) => {
-//   const movie = location.state;
-//   return (
-//     <section className="card-detail-section">
-//       <div className="movie-main">
-//         <img
-//           className="movie-backdrop"
-//           src={movie.backdrop_path}
-//           alt={movie.title + ' backdrop'}
-//         />
-//         <div className="overview-container">
-//           <label className="movie-overview">Overview</label>
-//           <p className="movie-description">{movie.overview}</p>
-//         </div>
-//       </div>
-//       <article className="movie-stats-container">
-//         <img
-//           className="movie-poster"
-//           src={movie.poster_path}
-//           alt={movie.title + ' poster'}
-//         />
-//         <div className="movie-stats">
-//           <h1 className="movie-title">{movie.title}</h1>
-//           <p className="movie-date">Release Date {movie.release_date}</p>
-//           <h3 className="movie-rating">
-//             Average Rating {movie.average_rating}
-//           </h3>
-//         </div>
-//       </article>
-//     </section>
-//   );
-// };
-
-// export default MovieDetails;
