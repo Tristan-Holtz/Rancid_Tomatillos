04e6c1cdc1686abf5d80fd048557873e4a0569a6
Kyle Cornelissen <46171611+kylecornelissen@users.noreply.github.com>
2020-02-23 21:36:30 -0700

    Add api call test for getMovies

--- src/apiCalls.test.js
+++ src/apiCalls.test.js
@@ -1,5 +1,5 @@
-import { getUser } from './apiCalls.js';
-import { shallow } from 'enzyme';
+import { getUser, getMovies } from './apiCalls.js';
+// import { shallow } from 'enzyme';
 
 describe('getUser', () => {
   const email = 'alan@turing.io';
@@ -51,5 +51,58 @@ describe('getUser', () => {
     })
 
     expect(getUser(email, password)).rejects.toEqual(Error('Error! No 200 Status Code.'));
+  });
+});
+
+describe('getMovies', () => {
+  const mockMovies = {
+    "movies": [
+      {
+        id: 1,
+        title: "Movie Title",
+        poster_path: "someURL",
+        backdrop_path: "someURL",
+        release_date: "2019-12-04",
+        overview: "Some overview",
+        average_rating: 6
+      }
+    ]
+  }
+
+  beforeEach(() => {
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.resolve({
+        ok: true,
+        json: () => Promise.resolve(mockMovies)
+      });
+    });
+  });
+
+  it('should call fetch with the correct url', () => {
+    getMovies();
+
+    expect(window.fetch).toHaveBeenCalledWith('https://rancid-tomatillos.herokuapp.com/api/v1/movies');
+  });
+
+  it('should return a resolved promise when response is ok', () => {
+    expect(getMovies()).resolves.toEqual(mockMovies);
+  });
+
+  it('should return error if the resolved promise response is not OK', () => {
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.resolve({
+        ok: false,
+        json: () => Promise.resolve(mockMovies)
+      });
+    });
+    expect(getMovies()).rejects.toEqual(Error('Error! No 200 Status Code.'));
+    expect(getMovies()).rejects.toThrow(Error('Error! No 200 Status Code.'));
+  });
+
+  it('should return error if the resolved promise response is not OK (Sad Path)', () => {
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.reject(Error("Error! No 200 Status Code."));
+    });
+    expect(getMovies()).rejects.toEqual(Error('Error! No 200 Status Code.'));
   });
 });
