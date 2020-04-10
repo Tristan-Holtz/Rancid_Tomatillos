2a99241018e66044066a4fb22e4ad89483802d01
Tristan Holtz <TristanHoltzConsulting@gmail.com>
2020-04-10 09:24:29 -0600

    Update trailers

--- src/trailers.js
+++ src/trailers.js
@@ -1,82 +1,162 @@
 export const trailers = [
   {
+    trailerID: 1,
+    src: 'https://www.youtube.com/embed/vOUVVDWdXbo',
+  },
+  {
+    trailerID: 2,
+    src: 'https://www.youtube.com/embed/szby7ZHLnkA',
+  },
+  {
+    trailerID: 3,
+    src: 'https://www.youtube.com/embed/gn5QmllRCn4',
+  },
+  {
+    trailerID: 4,
+    src: 'https://www.youtube.com/embed/jCFWEzIVILc',
+  },
+  {
+    trailerID: 5,
+    src: 'https://www.youtube.com/embed/bnGAgAgorC4',
+  },
+  {
+    trailerID: 6,
+    src: 'https://www.youtube.com/embed/yP86-TR6IME',
+  },
+  {
+    trailerID: 7,
+    src: 'https://www.youtube.com/embed/Ify9S7hj480',
+  },
+  {
+    trailerID: 8,
+    src: 'https://www.youtube.com/embed/WO_FJdiY9dA',
+  },
+  {
+    trailerID: 9,
+    src: 'https://www.youtube.com/embed/nOCDjU-frrg',
+  },
+  {
+    trailerID: 10,
+    src: 'https://www.youtube.com/embed/iQF1uhlQH5s',
+  },
+  {
+    trailerID: 11,
+    src: 'https://www.youtube.com/embed/5xH0HfJHsaY',
+  },
+  {
+    trailerID: 12,
+    src: 'https://www.youtube.com/embed/_wgHJ5gGOsw',
+  },
+  {
+    trailerID: 13,
+    src: 'https://www.youtube.com/embed/GVQbeG5yW78',
+  },
+  {
+    trailerID: 14,
+    src: 'https://www.youtube.com/embed/WhWlFeodMfI',
+  },
+  {
+    trailerID: 15,
+    src: 'https://www.youtube.com/embed/gZjQROMAh_s',
+  },
+  {
+    trailerID: 16,
+    src: 'https://www.youtube.com/embed/sowGYbxTPgU',
+  },
+  {
+    trailerID: 17,
+    src: 'https://www.youtube.com/embed/zjm6rDgKNMY',
+  },
+  {
+    trailerID: 18,
+    src: 'https://www.youtube.com/embed/5P8R2zAhEwg',
+  },
+  {
+    trailerID: 19,
+    src: 'https://www.youtube.com/embed/dFGYK-iRMgw',
+  },
+  {
+    trailerID: 20,
+    src: 'https://www.youtube.com/embed/l3fyOYx0BdA',
+  },
+  {
     trailerID: 21,
-    src: 'https://www.youtube.com/embed/szby7ZHLnkA'
+    src: 'https://www.youtube.com/embed/U3Xy2x9NDrw',
   },
   {
     trailerID: 22,
-    src: 'https://www.youtube.com/embed/isOGD_7hNIY'
+    src: 'https://www.youtube.com/embed/tt-ifhj7D64',
   },
   {
     trailerID: 23,
-    src: 'https://www.youtube.com/embed/f40JahDi1Uc'
+    src: 'https://www.youtube.com/embed/QG7cWYyNN8I',
   },
   {
     trailerID: 24,
-    src: 'https://www.youtube.com/embed/bwzLiQZDw2I'
+    src: 'https://www.youtube.com/embed/AST2-4db4ic',
   },
   {
     trailerID: 25,
-    src: 'https://www.youtube.com/embed/vUBmFqo0A_M'
+    src: 'https://www.youtube.com/embed/R_GFMbq4JPo',
   },
   {
     trailerID: 26,
-    src: 'https://www.youtube.com/embed/sL-9Khv7wa4'
+    src: 'https://www.youtube.com/embed/TZsgNH17_X4',
   },
   {
     trailerID: 27,
-    src: 'https://www.youtube.com/embed/mSBEWeXkMIk'
+    src: 'https://www.youtube.com/embed/rl33gU2APIs',
   },
   {
     trailerID: 28,
-    src: 'https://www.youtube.com/embed/rBxcF-r9Ibs'
+    src: 'https://www.youtube.com/embed/VzNJVSsjE-I',
   },
   {
     trailerID: 29,
-    src: 'https://www.youtube.com/embed/zyYgDtY2AMY'
+    src: 'https://www.youtube.com/embed/YnxHyBbYwQQ',
   },
   {
     trailerID: 30,
-    src: 'https://www.youtube.com/embed/gZjQROMAh_s'
+    src: 'https://www.youtube.com/embed/555oiY9RWM4',
   },
   {
     trailerID: 31,
-    src: 'https://www.youtube.com/embed/tL4McUzXfFI'
+    src: 'https://www.youtube.com/embed/LNaIewXwLHs',
   },
   {
     trailerID: 32,
-    src: 'https://www.youtube.com/embed/voYLots_ZOg'
+    src: 'https://www.youtube.com/embed/RfYAXMwCpk0',
   },
   {
     trailerID: 33,
-    src: 'https://www.youtube.com/embed/CmxEEE-KRpw'
+    src: 'https://www.youtube.com/embed/0rBnkBIhoFE',
   },
   {
     trailerID: 34,
-    src: 'https://www.youtube.com/embed/ZOWZusTSoes'
+    src: 'https://www.youtube.com/embed/lAxgztbYDbs',
   },
   {
     trailerID: 35,
-    src: 'https://www.youtube.com/embed/BVZDhunTrYA'
+    src: 'https://www.youtube.com/embed/YtPsxCxptqY',
   },
   {
     trailerID: 36,
-    src: 'https://www.youtube.com/embed/jKCj3XuPG8M'
+    src: 'https://www.youtube.com/embed/gYKnu-5UPMA',
   },
   {
     trailerID: 37,
-    src: 'https://www.youtube.com/embed/a6O30nJ02PU'
+    src: 'https://www.youtube.com/embed/hp33zFcvyRU',
   },
   {
     trailerID: 38,
-    src: 'https://www.youtube.com/embed/8Qn_spdM5Zg'
+    src: 'https://www.youtube.com/embed/5P8R2zAhEwg',
   },
   {
     trailerID: 39,
-    src: 'https://www.youtube.com/embed/vTfJp2Ts9X8'
+    src: 'https://www.youtube.com/embed/JggpSpqxS6I',
   },
   {
     trailerID: 40,
-    src: 'https://www.youtube.com/embed/N5yLcPzwPvk'
-  }
+    src: 'https://www.youtube.com/embed/ihovdBuP-tk',
+  },
 ];
